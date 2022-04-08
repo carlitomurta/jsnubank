3b1ff8b1073dbc4fd25ec348e5d6c41e1f7b94fe
Carlito Murta <carlitomurtaramalho@gmail.com>
2022-04-08 16:27:16 -0300

    removendo arquitetura antiga

--- src/utils/constants.ts
+++
@@ -1,4 +0,0 @@
-export const DISCOVERY_URL =
-  "https://prod-s0-webapp-proxy.nubank.com.br/api/discovery";
-export const DISCOVERY_APP_URL =
-  "https://prod-s0-webapp-proxy.nubank.com.br/api/app/discovery";
